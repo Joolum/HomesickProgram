639bf1ff9c46afb557f9d520a79c8fa8d594251f
JooLum <985136442@qq.com>
2023-03-06 18:20:00 +0800

    7777

--- verifyIP.py
+++ verifyIP.py
@@ -2,10 +2,14 @@ primitiveIP = "10.162.119.237"
 import os
 import re
 
-ipconfig = os.popen('ipconfig').read()
-currentIP = re.search(r'10\.162\.\d{0,127}\.\d{0,255}', ipconfig).group()
+ipConfig = os.popen('ipconfig').read()
+currentIP = re.search(r'10\.162\.\d{0,127}\.\d{0,255}', ipConfig).group()
+
+email_txt_ipChanged = 'IP Address has CHANGED! Analyzing domain name to a new IP...'
+
+print('Primitive IP: ' + primitiveIP)
 
 if currentIP != primitiveIP :
-    print('IP Address has CHANGED! Analyzing domain name to a new IP...')
+    print(email_txt_ipChanged)
 else:
-    print('CurrentIP: ' + currentIP + ". It's same as primitive IP!")
+    print('CurrentIP: ' + currentIP + ". It's same as the primitive IP!")
