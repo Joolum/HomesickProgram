d83081d579378eed36c74b833212eb04fcdb9d1d
KimBwn_L <985136442@qq.com>
2023-03-06 17:14:54 +0800

    任务1代码

--- pythonTask.py
+++ pythonTask.py
@@ -1,8 +1,6 @@
-import math
-
 def squre(side):
     squre_Side = side
-    squre_Area = math.sqrt(squre_Side)
+    squre_Area = squre_Side ** 2
     squre_Girth = squre_Side * 4
     print ('The squre side is %d' squre_Side)
     print ('The squre area is %d' squre_Area)
@@ -15,5 +13,7 @@ def exchangeNumber(firstNum,secondNum):
     print('First number is: %d. Second number is %d.' (firstNum,secondNum))
     print('Exchanged first number is: %d. Exchanged second number is %d'(firstNum,secondNum))
 
-def tringleSqure(s1,s2,s3):
-    
+def triangle(s1,s2,s3):
+    p = (s1 + s2 + s3) / 2
+    area = (p * (p - s1) * (p - s2) * (p - s3)) ** 0.5
+    print('The triangle area is %d' area)
