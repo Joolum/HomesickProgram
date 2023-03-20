40148f856996e66eab2a48a5c0e71e878078f220
KimBwn_L <985136442@qq.com>
2023-03-20 17:01:13 +0800

    quitequite

--- pythonTask.py
+++ pythonTask.py
@@ -1,13 +1,12 @@
 import math
 
 def q1(x):
-    match x:
-        case x<0:
-            return x*x
-        case 0<=x<5:
-            return 3*x-5
-        case x>=5:
-            return x/2-2
+    if x<0:
+        return x*x
+    elif 0<=x<5:
+        return 3*x-5
+    elif x>=5:
+        return x/2-2
 
 def q2(a,b,c):
     if a+b>c and a+c>b and b+c>a and a>0 and b>0 and c>0:
@@ -24,7 +23,8 @@ def q3():
         even = 0
         if i%2!=0:
             odd+=i
-        else even+=i
+        else:
+            even+=i
 
         print('Odd is: %d. Even is %d.' % (odd,even))
 
