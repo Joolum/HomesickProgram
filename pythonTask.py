d956769b29ae98c6d237ef12c55f7c764bd423d9
JooLum <985136442@qq.com>
2023-03-20 21:49:53 +0800

    最后一次作业修改

--- pythonTask.py
+++ pythonTask.py
@@ -1,40 +1,34 @@
 import math
 
-def squre(side):
-    squre_Side = side
-    squre_Area = squre_Side ** 2
-    squre_Girth = squre_Side * 4
-    print ('The squre side is %d' % squre_Side)
-    print ('The squre area is %d' % squre_Area)
-    print ('The squre girth is %d' % squre_Girth)
-    
-def exchangeNumber(firstNum,secondNum):
-    print('First number is: %d. Second number is %d.' % (firstNum,secondNum))
-    swap = firstNum
-    firstNum = secondNum
-    secondNum = swap
-    print('Exchanged first number is: %d. Exchanged second number is %d' % (firstNum,secondNum))
+def q1(x):
+    if x < 0:
+        return x * x
+    elif 0 <= x <= 5:
+        return 3 * x - 5
+    elif x >= 5:
+        return 0.5 * x - 2
 
-def triangle(s1,s2,s3):
-    p = float((s1 + s2 + s3) / 2)
-    area = (p * (p - s1) * (p - s2) * (p - s3)) ** 0.5
-    print('The triangle area is {:.2f}' .format(area))
+def q2(a, b, c):
+    if a + b > c > 0 and a + c > b > 0 and b + c > a > 0:
+        print("It is a triangle!")
+        p = (a + b + c) / 2
+        area = math.sqrt(p * (p - a) * (p - b) * (p - c))
+        print("Area is : %.2f" % area)
+    else:
+        print("It is not a triangle!")
 
-def judgeYear(year):
-    print((year % 4 == 0 & year % 100 != 0) or (year % 400 == 0))
+def q3():
+    odd = 0
+    even = 0
+    for i in range(1, 101):
+        if i % 2 != 0:
+            odd += i
+        else:
+            even += i
+    print('Odd is: %d. Even is %d.' % (odd, even))
 
-def mathPractice(x):
-    x = float(x)
-    y = math.sin(x) + 2 * math.sqrt(x + math.exp(4) - pow((x + 1), 3))
-    y = round(y, 2)
-    print('y=', y)
-
-squre(5)
-print('-----------------------')
-triangle(3,4,5)
-print('-----------------------')
-exchangeNumber(2,5)
-print('-----------------------')
-judgeYear(2023)
-print('-----------------------')
-mathPractice(2)
+print(q1(2))
+q2(3, 4, 5)
+q3()
+for i in range(0, 3):
+    print(q1(1))
